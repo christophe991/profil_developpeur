5756e5ca80933f3916ad0ff353727503f2ce8650
christophe991 <christophe.heddebaut@gmail.com>
2022-10-01 22:29:02 +0200

    affichage des outils

--- src/components/Competence.js
+++ src/components/Competence.js
@@ -20,11 +20,7 @@ const Competence = () => {
                             <div className="overflow-hidden">
                                 <table className="min-w-full">
                                     <thead className="border-b">
-                                        <tr>
-                                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left ">
-                                                Compétences en programmation
-                                            </th>
-                                        </tr>
+                                       <h3>Compétences en programmation</h3>
 
                                         <tr>
 
@@ -134,19 +130,20 @@ const Competence = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col w-96 ml-96  ">
+                <div className="flex flex-col w-96 ml-96 mt-20 ">
                     <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                             <div className="overflow-hidden">
                                 <table className="min-w-full">
                                     <thead className="border-b">
+                                    <h3>Outils utilisés</h3>
                                         <tr>
 
                                             <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left ">
                                                 Image
                                             </th>
                                             <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                Langage
+                                                Programme
                                             </th>
 
 
@@ -154,93 +151,78 @@ const Competence = () => {
                                     </thead>
                                     <tbody>
                                         <tr className="border-b ">
-                                            <img src="img/angular.png" className="h-20 ml-10 " alt="Logo angular " />
+                                            <img src="img/eclipse.png" className="h-20 ml-10 " alt="Logo angular " />
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                Angular
+                                                Eclipse
                                             </td>
 
 
                                         </tr>
                                         <tr className="bg-white border-b">
-                                            <img src="img/javascript.png" className="h-20 " alt="Logo javascript " />
+                                            <img src="img/github.png" className="h-20 " alt="Logo javascript " />
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                Javascript
+                                                GitHub
                                             </td>
 
 
                                         </tr>
                                         <tr className="bg-white border-b">
-                                            <img src="img/synfony.png" className="h-20 ml-3  " alt="Logo synfony " />
+                                            <img src="img/intellij.png" className="h-20 ml-3  " alt="Logo synfony " />
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                Synfony
+                                                Intellij
                                             </td>
 
 
                                         </tr>
                                         <tr className="bg-white border-b">
-                                            <img src="img/node.png" className="h-20 ml-3  " alt="Logo node " />
+                                            <img src="img/mongo.png" className="h-20 ml-3  " alt="Logo node " />
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                NodeJS
+                                                MongoDB
                                             </td>
 
 
                                         </tr>
                                         <tr className="bg-white border-b">
-                                            <img src="img/javafx.png" className="h-20 ml-3  " alt="Logo javafx " />
+                                            <img src="img/mysql.png" className="h-20 ml-3  " alt="Logo javafx " />
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                JavaFX
+                                                MYSQL
                                             </td>
 
 
                                         </tr>
                                         <tr className="bg-white border-b">
-                                            <img src="img/scenebuilder.png" className="h-20 ml-3  " alt="Logo scenebuilder " />
+                                            <img src="img/sourcetree.png" className="h-20 ml-3  " alt="Logo scenebuilder " />
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                SceneBuilder
+                                                SourceTree
                                             </td>
 
 
                                         </tr>
                                         <tr classNames="bg-white border-b">
-                                            <img src="img/react.png" className="h-20 ml-3  " alt="Logo react " />
+                                            <img src="img/sqlserver.png" className="h-20 ml-3  " alt="Logo react " />
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                React
+                                                SqlServer
                                             </td>
 
 
                                         </tr>
                                         <tr className="bg-white border-b">
-                                            <img src="img/vue.jpg" className="h-20 ml-3  " alt="Logo vue " />
+                                            <img src="img/trello.png" className="h-20 ml-3  " alt="Logo vue " />
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                VueJS
+                                                Trello
                                             </td>
 
 
                                         </tr>
                                         <tr className="bg-white border-b">
-                                            <img src="img/php.png" className="h-20 ml-3  " alt="Logo php " />
+                                            <img src="img/vscode.png" className="h-20 ml-3  " alt="Logo php " />
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                PHP
+                                                Visual Studio Code
                                             </td>
 
 
                                         </tr>
-                                        <tr className="bg-white border-b">
-                                            <img src="img/bootstrap.png" className="h-20 ml-3  " alt="Logo bootstrap " />
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                Bootstrap
-                                            </td>
-
-
-                                        </tr>
-                                        <tr className="bg-white border-b">
-                                            <img src="img/tailwind.png" className="h-20 ml-3  " alt="Logo tailwind " />
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                Tailwind
-                                            </td>
-
-
-                                        </tr>
+                                       
                                     </tbody>
                                 </table>
                             </div>
--- src/components/Creation.js
+++ src/components/Creation.js
@@ -1,10 +1,42 @@
 import React from 'react'
 
-const Creation = () =>{
+const Creation = () => {
     return (
-        <di>
+        <div>
             <h1>Création</h1>
-        </di>
+            <p>
+                Voici mes créations effectuer en formation et pendant mon loisir,
+                d'autre projet seront ajouter par la suite.
+
+            </p>
+            <h2>Mini jeu en javascript</h2>
+            <div>
+                <div class="flex justify-center">
+                    <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                        <a href="#!">
+                            <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt="" />
+                        </a>
+                        <div class="p-6">
+                            <h5 class="text-gray-900 text-xl font-medium mb-2 font-bold">Tetris</h5>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="flex justify-center">
+                    <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                        <a href="#!">
+                            <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt="" />
+                        </a>
+                        <div class="p-6">
+                            <h5 class="text-gray-900 text-xl font-medium mb-2 font-bold">Jeu de memoire</h5>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
