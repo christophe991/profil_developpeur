8d1eb42f5e424bbc1b152a0b919b36e370876e87
christophe991 <christophe.heddebaut@gmail.com>
2022-10-06 16:13:10 +0200

    continuation sur les experiences

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,23 +7,55 @@ const Experience = () => {
             <p>
                 Voici quelque création en projet de groupe
             </p>
-            <div class="flex justify-center">
+            <div class="flex justify-around">
+                <div class="rounded-lg shadow-lg bg-white max-w-sm">
+
+                    <div class="p-6">
+                        <h5 class="text-gray-900 text-xl mb-2 font-bold">Fouille-o-pot</h5>
+                        <hr></hr>
+                        <h3 class="text-gray-900 text-xl">Application en Java FX</h3>
+                        <p class="text-gray-900 text-xl  mb-2">
+                            Une application pour afficher des recettes avec les ingrédients
+                            et les ustensiles, possibilité d'ajouter de modifier ou de supprimer
+                            avec une gestion de frigo en selectionnant des l'ingrédients la liste de
+                            recette s'actualise selon l'ingrédient selectionner.
+                        </p>
+
+                    </div>
+                </div>
+                <div class="rounded-lg shadow-lg bg-white max-w-sm">
+
+                    <div class="p-6">
+                        <h5 class="text-gray-900 text-xl mb-2 font-bold">E-commerce</h5>
+                        <hr></hr>
+                        <h3 class="text-gray-900 text-xl">Site web en React et api en nodejs</h3>
+                        <p class="text-gray-900 text-xl  mb-2">
+                            Site web E-commerce, connexion commerçant, client,
+                            le commerçant peut ajouter un produit modifier ou supprimer 
+                            et gerer les question des clients.
+                        </p>
+
+                    </div>
+                </div>
+                
                     <div class="rounded-lg shadow-lg bg-white max-w-sm">
                         
                         <div class="p-6">
-                            <h5 class="text-gray-900 text-xl mb-2 font-bold">Fouille-o-pot</h5>
+                            <h5 class="text-gray-900 text-xl mb-2 font-bold"> Intranet</h5>
                             <hr></hr>
+                            <h3 class="text-gray-900 text-xl">Site veb en React et api en CSharp</h3>
                             <p class="text-gray-900 text-xl  mb-2">
-                                Une application pour afficher des recettes avec les ingrédients
-                                et les ustensiles, possibilité d'ajouter de modifier ou de supprimer
-                                avec une gestion de frigo en selectionnant des l'ingrédients la liste de 
-                                recette s'actualise selon l'ingrédient selectionner.
+                                Systeme de messagerie pour les entreprise,
+                                avec une gestion administratteur pour valider un message,
+                                connexion de l'utilisateur et possibilité de repondre au message.
                             </p>
 
                         </div>
                     </div>
-                </div>
+                
+            </div>
         </div>
+
     )
 
 }
